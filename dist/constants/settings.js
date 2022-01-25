ead3e0f02c06adc8c6be10c64c7efe5b20609991
markusbink <bink-markus@web.de>
2022-01-25 19:29:17 +0100

    Remove dist folder from repo
    Removes clutter from repository since it's not needed there

--- dist/constants/settings.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Setting = void 0;
-var Setting;
-(function (Setting) {
-    Setting["HYPERATE_ID"] = "HypeRate ID";
-})(Setting = exports.Setting || (exports.Setting = {}));
--- dist/constants/states.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.State = void 0;
-var State;
-(function (State) {
-    State["CURRENT_HEARTRATE"] = "hyperate_current_heartrate";
-})(State = exports.State || (exports.State = {}));
