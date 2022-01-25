2c715136fec6b3b49c08132ca0fe1dc66704fc97
markusbink <bink-markus@web.de>
2022-01-25 20:02:36 +0100

    Add dist back to the repo

---
+++ dist/constants/settings.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Setting = void 0;
+var Setting;
+(function (Setting) {
+    Setting["HYPERATE_ID"] = "HypeRate ID";
+})(Setting = exports.Setting || (exports.Setting = {}));
---
+++ dist/ws-controller.js
@@ -0,0 +1,56 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.WSController = void 0;
+var states_1 = require("./constants/states");
+var WSController = /** @class */ (function () {
+    function WSController(wss, tpService) {
+        this.wss = wss;
+        this.tpService = tpService;
+    }
+    /**
+     * Conntects to the Hyperate WebSocket
+     * @param hypeRateUserId HypeRate User ID
+     */
+    WSController.prototype.join = function (hypeRateUserId) {
+        var payload = {
+            topic: "hr:" + hypeRateUserId,
+            event: "phx_join",
+            payload: {},
+            ref: 0,
+        };
+        this.wss.send(JSON.stringify(payload));
+    };
+    /**
+     * Sends a heartbeat to the Hyperate WebSocket
+     * to keep connection alive
+     */
+    WSController.prototype.sendHeartbeat = function () {
+        var _this = this;
+        var payload = {
+            topic: "phoenix",
+            event: "heartbeat",
+            payload: {},
+            ref: 0,
+        };
+        setInterval(function () {
+            _this.wss.send(JSON.stringify(payload));
+        }, 25000);
+    };
+    /**
+     * Callback for when a message is received from the Hyperate WebSocket
+     * @param payload Payload received from the Hyperate WebSocket
+     */
+    WSController.prototype.onMessage = function (payload) {
+        var message = JSON.parse(payload);
+        switch (message.event) {
+            case "hr_update":
+                var heartRate = message.payload.hr;
+                this.tpService.updateState(states_1.State.CURRENT_HEARTRATE, heartRate);
+                break;
+            default:
+                console.log("Message type does not exist");
+        }
+    };
+    return WSController;
+}());
+exports.WSController = WSController;
