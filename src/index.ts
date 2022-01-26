d29f3f9ba5367692d4a1a3d6ccd7ec6512eb6481
markusbink <bink-markus@web.de>
2022-01-26 20:19:56 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,18 +10,20 @@ dotenv.config();
 (async () => {
   const HYPERATE_WEBSOCKET_URL = `wss://staging.frightrate.com/socket/websocket?token=${process.env.HYPERATE_API_KEY}`;
   const TOUCHPORTAL_PLUGIN_ID = "markusbink.TouchPortalHypeRatePlugin";
+  try {
+    const tpService = new TPController(TOUCHPORTAL_PLUGIN_ID);
+    const wss = new WebSocket(HYPERATE_WEBSOCKET_URL);
+    const wsController = new WSController(wss, tpService);
 
-  console.log({ HYPERATE_WEBSOCKET_URL });
+    const hypeRateUserId = await tpService.getSettingByKey(Setting.HYPERATE_ID);
 
-  const tpService = new TPController(TOUCHPORTAL_PLUGIN_ID);
-  const wss = new WebSocket(HYPERATE_WEBSOCKET_URL);
-  const wsController = new WSController(wss, tpService);
-
-  const hypeRateUserId = await tpService.getSettingByKey(Setting.HYPERATE_ID);
-
-  if (wss) {
-    wss.on("open", () => wsController.join(hypeRateUserId));
-    wsController.sendHeartbeat();
-    wss.on("message", (payload) => wsController.onMessage(payload));
+    if (wss) {
+      wss.on("open", () => wsController.join(hypeRateUserId));
+      wsController.sendHeartbeat();
+      wss.on("message", (payload) => wsController.onMessage(payload));
+    }
+  } catch (err) {
+    console.error("Error: " + HYPERATE_WEBSOCKET_URL);
+    console.error(err);
   }
 })();
